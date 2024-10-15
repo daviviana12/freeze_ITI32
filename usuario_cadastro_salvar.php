1bb2243c3959e2aefd231b0687fccaf01b9c0849
daviviana12 <contadeacesso465@gmail.com>
2024-10-15 11:50:03 -0300

    Codificando a criação do usuário num BD

--- usuario_cadastro_salvar.php
+++ usuario_cadastro_salvar.php
@@ -6,10 +6,33 @@
     <title>Document</title>
     <?php include "referencias.php" ?>
 </head>
+
 <body>
     <!---Incluindo o cabeçalho da página-->
 <?php include "header_admin.php" ?>
-    <!---Incluindo o rodapé da página-->
+<?php
+
+include "conexao_bd.php";
+
+$login_usuario = $_POST["txtLoginUsuario"];
+$senha_usuario = $_POST["txtSenhaUsuario"];
+
+$hash = password_hash($senha_usuario,1);
+
+$sql = "INSERT INTO usuario(login_usuario,senha_usuario) ";
+$sql .= " VALUES('$login_usuario','$hash')";
+
+if (executarComando($sql))
+{
+    echo "<h1>Usuário adicionado!</h1>";
+
+}
+else
+{
+    echo "<h1>Usuário não adicionado!</h1>";
+}
+?>
+<!---Incluindo o rodapé da página-->
 <?php include "footer_admin.php" ?>
 </body>
 </html>
